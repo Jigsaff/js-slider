810d70d8aa336b3bf00f808f421fabd3214ca55b
Stanislav Rugalenko <jigsaff@gmail.com>
2021-01-13 20:22:36 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let offset = 0;
+const sliderLine = document.querySelector(".slider-line");
+
+document.querySelector(".slider-next").addEventListener("click", function () {
+    offset = offset + 512;
+    if (offset > 1024) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + "px";
+});
+
+document.querySelector(".slider-prev").addEventListener("click", function () {
+    offset = offset - 512;
+    if (offset < 0) {
+        offset = 1024;
+    }
+    sliderLine.style.left = -offset + "px";
+    console.log(offset)
+});
